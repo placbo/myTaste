69aea7236683091b8c816370b92c3e29c1aa0a54
pcb <perbjester@gmail.com>
2020-03-04 23:38:01 +0100

    started splitting server.js

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("./model/user");
+const Rating = require("./model/rating");
 const router = express.Router();
 
 router.get("/test", (req, res) => {
@@ -15,5 +16,22 @@ router.get("/", (req, res) => {
     res.status(500).json({ data: null, error: { subject: "server" }, message: err.toString() });
   });
 });
+
+router.get("/mytasteapi/rating2/:itemId/:userId", (req, res) => {
+  console.log(
+      `Fetch rating (GET) userId: ${req.params.userId}, itemId: ${req.params.itemId} `
+  );
+  Rating.find({ itemId: req.params.itemId, userId: req.params.userId },
+      (err, result) => {
+        if (err) {
+          console.log("ERROR: " + err.message);
+          res.status(res.code || 500).json({ error: "Failed to get rating"});
+        } else {
+          res.status(200).json(result);
+        }
+      }
+  );
+});
+
 
 module.exports = router;
--- server/testserver.js
+++ server/testserver.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const routes = require("./routes");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Items = require("./model/item");
+const User = require("./model/user");
+const Rating = require("./model/rating");
 
-
-mongoose.connect('mongodb://localhost:27017/mytaste', {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/mytaste", {
+  useNewUrlParser: true
+});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log("mongodb connected");
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("mongodb connected");
 });
 
 const app = express();
 
-app.listen(3030, ()=> {
-    console.log("Test-application running om port 3030")
+app.listen(3030, () => {
+  console.log("Test-application running om port 3030");
 });
 
 app.use(routes);
 
-
-
+Items.find({}, (err, result) => {
+  console.log(result);
+});
