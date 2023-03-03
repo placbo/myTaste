5522209ebc2c839a4992543e131d31b1a5faf589
PCB <perbjester@gmail.com>
2023-03-03 22:09:12 +0100

    Updates

---
+++ precache-manifest.1aeedd21bf59eba801689c68f86b4372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8504b0bc6e778bec5275db8702d14a1a",
+    "url": "/myTaste/index.html"
+  },
+  {
+    "revision": "6b8aa5244701ea600a74",
+    "url": "/myTaste/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "6b8aa5244701ea600a74",
+    "url": "/myTaste/static/js/2.3fd11145.chunk.js"
+  },
+  {
+    "revision": "48b9140d67fff040c0a668b5deccd6c5",
+    "url": "/myTaste/static/js/2.3fd11145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c745b11e228ba3322f0",
+    "url": "/myTaste/static/js/main.f6a70575.chunk.js"
+  },
+  {
+    "revision": "c344c150dbda0fa1c485",
+    "url": "/myTaste/static/js/runtime-main.6166c347.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3c6ba3bfd48fe09ac6ce4417fc72d18.js"
+  "/myTaste/precache-manifest.1aeedd21bf59eba801689c68f86b4372.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myTaste/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
